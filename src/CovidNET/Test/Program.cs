44f77033ec7fc2f4eb643cbebd5a5536fd6cde72
Tornike <t.gomareli@upcast.ge>
2020-04-09 01:18:35 +0400

    refactored and put comments in sample proj

--- src/CovidNET/Test/Program.cs
+++ src/CovidNET/Test/Program.cs
@@ -4,25 +4,36 @@ using CovidNET_lib;
 using CovidNET_lib.Http;
 using Newtonsoft.Json;
 
+// WARNING!
+// you should call every async method with await and asynchronously
+// because we are in Main, I am calling them syncrhonously.
+// Never do it in production
+
 namespace CovidNETSamples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var current = new CovidNetClient();
-            current.InitCovidDataAsync().Wait();
+            var covidClient = new CovidNetClient();
+            covidClient.InitCovidDataAsync().Wait(); 
 
             // exception is happening
             try
             {
-                var s = current.GetGlobalInfoByDateAsync(new DateTime(2020, 03, 03).Date).Result;
-                var s1 = current.GetLatestGlobalInfoAsync().Result;
-                var s2 = current.GetCountryTimeSeries("Georgia", new DateTime(2020, 02, 03), new DateTime(2020, 03, 04));
-                var s3 = current.GetCurrentAllCovidCountryStatsAsync().Result;
-                var s4 = current.GetCurrentCovidInfoByCountryAsync("Georgia").Result;
-                var s5 = current.GetCovidCountryInfoByDateAsync("Georgia", new DateTime(2020, 03, 03)).Result;
-                var s6 = current.GetCountryTimeSeriesByName("Georgia");
+                var globalInfoByDate = covidClient.GetGlobalInfoByDateAsync(new DateTime(2020, 03, 03).Date).Result;
+
+                var latestGlobalInfo = covidClient.GetLatestGlobalInfoAsync().Result;
+
+                var timeSeries = covidClient.GetCountryTimeSeries("Georgia", new DateTime(2020, 02, 03), new DateTime(2020, 03, 04));
+
+                var allCovidCountriesInfo = covidClient.GetCurrentAllCovidCountryStatsAsync().Result;
+
+                var covidInfoOnSpecificCountry = covidClient.GetCurrentCovidInfoByCountryAsync("Georgia").Result;
+
+                var covidCountryInfoByDate = covidClient.GetCovidCountryInfoByDateAsync("Georgia", new DateTime(2020, 03, 03)).Result;
+
+                var countryTimeSeries = covidClient.GetCountryTimeSeriesByName("Georgia");
             }
             catch(Exception ex)
             {
