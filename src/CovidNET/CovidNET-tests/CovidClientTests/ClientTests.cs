764a7759b854c9630d350c18fdfe0d45c2749277
Tornike <t.gomareli@upcast.ge>
2020-04-09 18:22:07 +0400

    some tests added

--- src/CovidNET/CovidNET-tests/CovidClientTests/ClientTests.cs
+++ src/CovidNET/CovidNET-tests/CovidClientTests/ClientTests.cs
@@ -1,18 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using CovidNET_lib;
+using CovidNET_lib.Exceptions;
 using NUnit.Framework;
 
 namespace CovidNET_tests.CovidClientTests
 {
     public class ClientTests
     {
+        CovidNetClient _covidClient;
         [SetUp]
-        public void Setup()
+        public async Task Setup()
         {
+            _covidClient = new CovidNetClient();
+            await _covidClient.InitCovidDataAsync();
         }
 
         [Test]
-        public void Test1()
+        public void GetCountryTimeSeries_throwsException_if_date_is_less_then_22January()
         {
-            Assert.Pass();
+            var dateTime = new DateTime(2020, 1, 3);
+
+
+            Assert.Throws<AggregateException>(() => 
+			{
+                var result = _covidClient.GetGlobalInfoByDateAsync(dateTime).Result;
+            });
+        }
+
+        [Test]
+        public void GetCountryTimeSeriesByName_throwsException_if_string_is_null()
+        {
+            string countryName = null;
+
+
+            Assert.Throws<StringIsNullOrEmptyException>(() =>
+            {
+                var result = _covidClient.GetCountryTimeSeriesByName(countryName);
+            });
+        }
+
+        [Test]
+        public void GetCountryTimeSeriesByName_throwsException_if_string_is_empty()
+        {
+            string countryName = string.Empty;
+
+
+            Assert.Throws<StringIsNullOrEmptyException>(() =>
+            {
+                var result = _covidClient.GetCountryTimeSeriesByName(countryName);
+            });
         }
     }
 }
